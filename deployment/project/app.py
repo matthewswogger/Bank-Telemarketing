f84585ea2ad40fdff9640b95da58a05f13294e7a
Matthew Swogger May <msmay@Matthews-MBP.lan1>
2020-12-01 11:22:00 -0500

    need new branch to test a thought

--- deployment/project/app.py
+++ deployment/project/app.py
@@ -32,145 +32,62 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 
+
 @st.cache
 def getData():
-    return pd.read_csv('/usr/src/app/data/test.csv')
+    df = pd.read_csv('/usr/src/app/data/test.csv')
+    X = df.drop(columns='y')
+    y = df.y
+    return X, y
 
 
-_max_width_()
-
-st.title('Vendor Quality Tracking')
-
-df = getData()
-
-st.write(df.head())
-
-X_holdout = df.drop(columns='y')
-y_holdout = df.y
+def loadModel():
+    return joblib.load('/usr/src/app/saved_models/random_forest_feature_selected.joblib')
 
 
-clf = joblib.load('/usr/src/app/saved_models/random_forest_feature_selected.joblib')
+def main():
+    # _max_width_()
+    choose_model = st.sidebar.selectbox("Choose the ML Model", ["NONE","Decision Tree", "Neural Network", "K-Nearest Neighbours"])
 
-c_report = classification_report(y_holdout, clf.predict(X_holdout))
-st.text(c_report)
+    st.title('Vendor Quality Tracking')
 
-# fig = px.histogram(data['Start station'], x ='Start station')
-# st.plotly_chart(fig)
+    X_holdout, y_holdout = getData()
+    st.write(X_holdout.head())
 
-cm = confusion_matrix(y_holdout, clf.predict(X_holdout))
-cm = pd.DataFrame(cm, columns=['no', 'yes'], index=['no', 'yes'])
-cm.columns.name = 'Predicted'
-cm.index.name = 'True'
+    clf = loadModel()
 
-cm_norm = (cm.T/cm.T.sum()).T
+    c_report = classification_report(y_holdout, clf.predict(X_holdout))
+    st.text(c_report)
 
-cm_norm
+    # fig = px.histogram(data['Start station'], x ='Start station')
+    # st.plotly_chart(fig)
 
-visitors_array = cm_norm.round(3).to_numpy()[::-1, :]
+    cm = confusion_matrix(y_holdout, clf.predict(X_holdout))
+    cm = pd.DataFrame(cm, columns=['no', 'yes'], index=['no', 'yes'])
+    cm.columns.name = 'Predicted'
+    cm.index.name = 'True'
 
-#display(nparray)
-Weekdays_list = ['no', 'yes'] 
-Hours_list = ['yes', 'no']
+    cm_norm = (cm.T/cm.T.sum()).T
 
-layout_heatmap = go.Layout(
-    title=('Confusion Matrix'),
-    xaxis=dict(title='Predicted'), 
-    yaxis=dict(title='True', dtick=1)
-)
+    visitors_array = cm_norm.round(3).to_numpy()[::-1, :]
 
-ff_fig = ff.create_annotated_heatmap(x= Weekdays_list, y=Hours_list, z=visitors_array, showscale = True, colorscale='Blues')
-fig  = go.FigureWidget(ff_fig)
-fig.layout=layout_heatmap
-fig.layout.annotations = ff_fig.layout.annotations
-fig.data[0].colorbar = dict(title='', titleside = 'right')
-st.plotly_chart(fig)
 
-# df = pd.read_csv('/usr/src/app/data/two_vendor_differences.csv')
+    Weekdays_list = ['no', 'yes']
+    Hours_list = ['yes', 'no']
 
-# vendor = st.selectbox(
-#     'Which Vendor would you like to inspect?',
-#     ('Vendor 1', 'Vendor 2')
-# )
-# st.write('You selected:', vendor)
+    layout_heatmap = go.Layout(
+        title=('Confusion Matrix'),
+        xaxis=dict(title='Predicted'),
+        yaxis=dict(title='True', dtick=1)
+    )
 
-# fig = go.Figure()
+    ff_fig = ff.create_annotated_heatmap(x= Weekdays_list, y=Hours_list, z=visitors_array, showscale = True, colorscale='Blues')
+    fig  = go.FigureWidget(ff_fig)
+    fig.layout=layout_heatmap
+    fig.layout.annotations = ff_fig.layout.annotations
+    fig.data[0].colorbar = dict(title='', titleside = 'right')
+    st.plotly_chart(fig)
 
-# if vendor == 'Vendor 1':
-#     for row in df.itertuples():
-#         fig.add_scatter3d(
-#             x=[row.x, row.x_vendor1],
-#             y=[row.y, row.y_vendor1],
-#             z=[row.z, row.z_vendor1],
-#             showlegend=False,
-#             marker=dict(
-#                 size=1,
-#                 color='cyan',
-#             ),
-#             line=dict(
-#                 width=10,
-#                 color='cyan',
-#             ),
-#         )
 
-#     fig.add_scatter3d(
-#         x=df.x_vendor1,
-#         y=df.y_vendor1,
-#         z=df.z_vendor1,
-#         showlegend=False,
-#         marker=dict(
-#             size=((df.x_diff_vendor1**2 + df.y_diff_vendor1**2 + df.z_diff_vendor1**2)**.5),
-#             color='red',
-#         ),
-#     )
-# else:
-#     for row in df.itertuples():
-#         fig.add_scatter3d(
-#             x=[row.x, row.x_vendor2],
-#             y=[row.y, row.y_vendor2],
-#             z=[row.z, row.z_vendor2],
-#             showlegend=False,
-#             marker=dict(
-#                 size=1,
-#                 color='cyan',
-#             ),
-#             line=dict(
-#                 width=10,
-#                 color='cyan',
-#             ),
-#         )
-
-#     fig.add_scatter3d(
-#         x=df.x_vendor2,
-#         y=df.y_vendor2,
-#         z=df.z_vendor2,
-#         showlegend=False,
-#         marker=dict(
-#             size=((df.x_diff_vendor2**2 + df.y_diff_vendor2**2 + df.z_diff_vendor1**2)**.5),
-#             color='red',
-#         ),
-#     )
-
-# fig.add_scatter3d(
-#     x=df.x,
-#     y=df.y,
-#     z=df.z,
-#     showlegend=False,
-#     marker=dict(
-#         size=4,
-#         color='midnightblue',
-#     ),
-# )
-
-# fig.update_layout(
-#     autosize=False,
-#     width=1600,
-#     height=800,
-#     scene_camera_eye=dict(
-#         x=3,
-#         y=3,
-#         z=3
-#     )
-# )
-
-# st.plotly_chart(fig)
-
+if __name__ == "__main__":
+    main()
